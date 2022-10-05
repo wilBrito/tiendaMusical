f01860c1a32f10cda66739718d4067cc4620b08a
Wilson Brito <wil.brito@icloud.com>
2022-10-05 12:37:31 +0200

    version inicial de tienda musical

---
+++ tiendaMusical/tiendaMusical-data/src/main/java/org/tiendaMusical/data/App.java
@@ -0,0 +1,13 @@
+package org.tiendaMusical.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tiendaMusical/tiendaMusical-services/src/main/java/org/tiendaMusical/services/App.java
@@ -0,0 +1,13 @@
+package org.tiendaMusical.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
